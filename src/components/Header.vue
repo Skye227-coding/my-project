df594cf8cd083d5050d5408d7d44eac739db921c
Skye Liu <skyeliu@Skyes-MacBook-Pro.local>
2020-05-21 00:30:04 +0800

    first draft of home page

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="tab-menu">
+        <!-- v-model绑定的是name value -->
+        <el-tabs v-model="category" @tab-click="changeCategory">
+          <el-tab-pane label="全部" name="all"></el-tab-pane>  
+          <el-tab-pane label="蔬菜" name="vege"></el-tab-pane>
+          <el-tab-pane label="水果" name="fruit"></el-tab-pane>
+          <el-tab-pane label="肉禽蛋" name="meat"></el-tab-pane>
+          <el-tab-pane label="海鲜" name="seafood"></el-tab-pane>
+          <el-tab-pane label="调味品" name="spices"></el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+<script>
+  export default {
+    name:'Header',
+    data() {
+      return {
+        category: 'all'
+      };
+    },
+    methods: {
+      changeCategory() {
+        this.$store.state.goodCategory=this.category
+        console.log(this.category)
+      }
+    }
+  };
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <app-header></app-header>
+        <p>This is Home page</p>
+    </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Header from '../components/Header'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    data(){
+        return {
+            goods:[]
+        }
+    },
+    components:{
+        appHeader:Header
+    },
+    mounted(){
+        this.axios.get('/data/goods.json')
+          .then(res=>{
+            this.goods = [...res.data];
+          })
+          .catch(()=>{
+              console.log("error");
+          })
+    }
+    
 }
 </script>
